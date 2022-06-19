d6c3a2cac4892af377eba6851daafd98ca038e71
YYZY <1101610062@qq.com>
2022-06-20 02:01:33 +0800

    ..

---
+++ src/test/java/com/yyzy/ondriveweb/OnedriveWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyzy.ondriveweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnedriveWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
